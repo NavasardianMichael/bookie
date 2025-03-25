d422f1d219479554587d44d3e6775110b2011b3b
@michaelnavasardyan <michaelnavasardyan@espoc.com>
2025-03-25 23:04:29 +0400

    trying to deploy on vercel

--- src/app/providers/[providerId]/page.tsx
+++ src/app/providers/[providerId]/page.tsx
@@ -1,22 +1,22 @@
-
 import { Provider as ProviderType } from '@interfaces/provider'
 
 // export const getStaticProps = (async (context) => {
 //     return { props: MOCK_PROVIDERS.find(provider => context.params?.providerId === provider.id)! }
 // }) satisfies GetStaticProps<ProviderType>
 
-
 type Context = {
-    params: {
+    params: Promise<{
         providerId: ProviderType['id']
-    }
+    }>
 }
 
-const Provider = async (context: Context) => {
-    const params = await context.params
-    return <article>
-        <header>Provider Id: {params.providerId}</header>
-    </article>
+const Provider = async ({ params }: Context) => {
+    const { providerId } = await params
+    return (
+        <article>
+            <header>Provider Id: {providerId}</header>
+        </article>
+    )
 }
 
 export default Provider
