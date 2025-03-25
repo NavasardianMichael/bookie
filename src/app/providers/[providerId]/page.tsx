38461fda2a7f3d970e2ab1b896499376682c6f01
NavasardianMichael <michaelnavasardyan@espoc.com>
2025-03-25 04:25:51 +0400

    just provider id is printed on the provider dynamic page

--- src/app/providers/[providerId]/page.tsx
+++ src/app/providers/[providerId]/page.tsx
@@ -1,16 +1,21 @@
+
 import { Provider as ProviderType } from '@interfaces/provider'
-import { MOCK_PROVIDERS } from '@mock/providers'
 
-import type { GetStaticProps, InferGetStaticPropsType } from 'next'
-
-export const getStaticProps = (async (context) => {
-    return { props: MOCK_PROVIDERS.find(provider => context.params?.providerId === provider.id)! }
-}) satisfies GetStaticProps<ProviderType>
+// export const getStaticProps = (async (context) => {
+//     return { props: MOCK_PROVIDERS.find(provider => context.params?.providerId === provider.id)! }
+// }) satisfies GetStaticProps<ProviderType>
 
 
-const Provider = async (params: InferGetStaticPropsType<typeof getStaticProps>) => {
+type Context = {
+    params: {
+        providerId: ProviderType['id']
+    }
+}
+
+const Provider = async (context: Context) => {
+    const params = await context.params
     return <article>
-        <h2>{params.name}</h2>
+        <header>Provider Id: {params.providerId}</header>
     </article>
 }
 
