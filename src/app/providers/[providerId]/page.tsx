642577cf32be1b41c373dd68fc685f22e5669c91
NavasardianMichael <michaelnavasardyan@espoc.com>
2025-03-25 04:21:17 +0400

    trying to get providerId dynamically

--- src/app/providers/[providerId]/page.tsx
+++ src/app/providers/[providerId]/page.tsx
@@ -1,12 +1,17 @@
 import { Provider as ProviderType } from '@interfaces/provider'
-import { GetStaticProps } from 'next';
+import { MOCK_PROVIDERS } from '@mock/providers'
 
-const Provider = async (params: GetStaticProps<ProviderType>) => {
-    console.log({ params });
+import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 
-    return (
-        <article>{params.name}</article>
-    )
+export const getStaticProps = (async (context) => {
+    return { props: MOCK_PROVIDERS.find(provider => context.params?.providerId === provider.id)! }
+}) satisfies GetStaticProps<ProviderType>
+
+
+const Provider = async (params: InferGetStaticPropsType<typeof getStaticProps>) => {
+    return <article>
+        <h2>{params.name}</h2>
+    </article>
 }
 
 export default Provider
