66386c7435f96d9b6b5596cf0c4619c8f8393271
@michaelnavasardyan <michaelnavasardyan@espoc.com>
2025-03-25 23:51:54 +0400

    v4: dynamic route metadata added

--- src/app/providers/[providerId]/page.tsx
+++ src/app/providers/[providerId]/page.tsx
@@ -1,10 +1,24 @@
 import { Provider as ProviderType } from '@interfaces/provider'
+import { Metadata, ResolvingMetadata } from 'next'
 
-type Params = Promise<{
-    providerId: ProviderType['id']
-}>
+type Props = {
+    params: Promise<{
+        providerId: ProviderType['id']
+    }>
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
 
-const Provider = async ({ params }: { params: Params }) => {
+export const generateMetadata = async ({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> => {
+    const { providerId } = await params
+    console.log(await parent)
+
+    return {
+        title: `Provider page with an id ${providerId}`,
+        description: `ProviderId => ${providerId}`
+    }
+}
+
+const Provider = async ({ params }: Props) => {
     const { providerId } = await params
     return (
         <article>
